1f33946fdc62f1241646f4b7bd2147f8bd4ecc82
saltpy <saltpy@gmail.com>
2014-02-05 10:21:38 +0000

    Fixed: headless eclim install

--- install.sh
+++ install.sh
@@ -16,10 +16,13 @@ cp -R vim ~/.vim
 
 . ~/.bashrc
 
-# eclim headed
+# eclim headless
 sudo apt-get update
 sudo apt-get install openjdk-6-jdk build-essential -y
 sudo apt-get install eclipse -y
 sudo apt-get install maven -y
 curl -LO http://downloads.sourceforge.net/project/eclim/eclim/1.7.14/eclim_1.7.14.jar
-java -Dvim.files=~/.vim -Declipse.home=$ECLIPSE_HOME -jar eclim_1.7.14.jar install
+java \
+-Dvim.files=$HOME/.vim \
+-Declipse.home=$ECLIPSE_HOME \
+-jar eclim_1.7.14.jar install
