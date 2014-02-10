fe606060d60f3dee28848d1e4f86a204016fb466
saltpy <saltpy@gmail.com>
2014-02-10 13:02:50 +0000

    Added: eclim setup now optional with --with-eclim

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ git pull origin master
 git submodule init
 git submodule update
 
+sudo apt-get update
+sudo apt-get install curl -y
 mkdir -p vim/autoload
 curl -Sso vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
@@ -17,7 +19,8 @@ cp -R vim ~/.vim
 . ~/.bashrc
 
 # eclim headless
-sudo apt-get update
+if [ "$1" == "--with-eclim" ]
+then
 sudo apt-get install default-jdk xvfb build-essential -y
 sudo apt-get install eclipse -y
 sudo apt-get install maven -y
@@ -26,3 +29,4 @@ java \
 -Dvim.files=$HOME/.vim \
 -Declipse.home=$ECLIPSE_HOME \
 -jar eclim_1.7.14.jar install
+fi
