e63eeeda192fea4090cfabc588bba766ecc4b6d6
saltpy <saltpy@gmail.com>
2014-02-11 10:13:26 +0000

    Added: script for getting node 0.10.x on Ubuntu LTS

---
+++ upnode.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install -y python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
