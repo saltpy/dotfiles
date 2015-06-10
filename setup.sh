bc498b9aabbb9082b0d2453a0a0a84335e7160ea
James Salt <saltpy@gmail.com>
2015-06-10 16:32:10 +0100

    Manage ssh agent in bashrc

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-mkdir ~/src
+mkdir -p ~/src
 
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/cocoon_id_rsa
+sudo apt-get install -y build-essential python-dev git python-setuptools python-dev python-crypto python-pip python-virtualenv
+
 git config --global user.name "James Salt"
 git config --global user.email "saltpy@gmail.com"
+git config --global core.autoclrf false
 
 cd src
-git clone git@github.com/saltpy/dotfiles.git
-./install.sh
+git clone http://github.com/saltpy/dotfiles.git
+  cd dotfiles
+  ./install.sh
+  cd -
 cd -
-
-sudo apt-get install -y python-setuptools python-dev python-crypto python-pip python-virtualenv
