0c23c22cf8b558970767a274d8243747d934d820
James Salt <saltpy@gmail.com>
2015-06-11 16:44:33 +0100

    Add some conventional folder stuff and fabric

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/src
+mkdir -p ~/src ~/bin ~/lib ~/doc ~/art
 
 sudo apt-get update
 sudo apt-get fix-missing
 sudo apt-get install -y build-essential python-dev git python-setuptools python-dev python-crypto python-pip python-virtualenv
+
+virtualenv ~/fab
+. ~/fab/bin/activate
+pip install fabric
+deactivate
 
 git config --global user.name "James Salt"
 git config --global user.email "saltpy@gmail.com"
