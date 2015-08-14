5c582301cf323f859aae98be2ecd5d56e6c54185
Ubuntu <ubuntu@ip-172-31-1-160.eu-west-1.compute.internal>
2015-08-14 10:12:01 +0000

    Move to using pushd, popd and ssh keys

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
 rm -rf ~/src ~/bin ~/lib ~/doc ~/art ~/env
-mkdir -p ~/src ~/bin ~/lib ~/doc ~/art ~/env
+mkdir -p ~/src ~/bin ~/lib ~/doc ~/art ~/env ~/www ~/log
 
 sudo apt-get update --fix-missing
-sudo apt-get install -y vim build-essential python-dev git python-setuptools python-dev python-crypto python-pip python-virtualenv
-
-virtualenv ~/env/fab
-. ~/env/fab/bin/activate
-pip install fabric
-deactivate
+sudo apt-get install -y vim build-essential python-dev git python-virtualenv
 
 git config --global user.name "James Salt"
 git config --global user.email "saltpy@gmail.com"
 git config --global core.autoclrf false
 
-git clone http://github.com/saltpy/dotfiles.git src/dotfiles
-bash src/dotfiles/install.sh
+pushd ~/src
+git clone git@github.com:saltpy/dotfiles
+pushd dotfiles
+bash install.sh
+popd
+popd
--- install.sh
+++ install.sh
@@ -4,15 +4,21 @@ rm -f ~/.bashrc
 rm -f ~/.vimrc
 rm -rf ~/.vim
 
-cp ~/src/dotfiles/bashrc ~/.bashrc
-cp ~/src/dotfiles/vimrc ~/.vimrc
+mkdir -p ~/.vim
+mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/autoload
 
-mkdir ~/.vim
-mkdir ~/.vim/bundle
-mkdir ~/.vim/autoload
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-cd ~/.vim/bundle
-git clone git://github.com/kien/ctrlp.vim.git
-git clone git://github.com/altercation/vim-colors-solarized.git
-git clone git://github.com/ervandew/supertab.git
-cd -
+pushd ~/src/dotfiles
+cp bashrc ~/.bashrc
+cp vimrc ~/.vimrc
+popd
+
+pushd ~/.vim/autoload
+curl -LSso pathogen.vim https://tpo.pe/pathogen.vim
+popd
+
+pushd ~/.vim/bundle
+git clone git@github.com:kien/ctrlp.vim
+git clone git@github.com:altercation/vim-colors-solarized
+git clone git@github.com:ervandew/supertab
+popd
