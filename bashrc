46413b559940ee7fd20c7ea088b47a7ebcb84270
James Salt <james.salt@williamhill.com>
2017-11-21 13:48:34 +0000

    Add bash ssh-agent

--- bashrc
+++ bashrc
@@ -4,6 +4,11 @@ HISTCONTROL=ignoredups
 HISTSIZE=
 HISTFILESIZE=
 
+if [ -z "$SSH_AUTH_SOCK" ] ; then
+    eval `ssh-agent -s`
+    ssh-add
+fi
+
 . ~/.bash_prompt
 . ~/.bash_alias
 . ~/.inputrc
--- install.py
+++ install.py
@@ -11,11 +11,18 @@ HOME = os.path.expanduser(os.environ["HOME"])
 SRC = "/srv/src/dotfiles"
 if not os.path.exists(SRC):
     with open("/dev/null", "w") as devnull:
-        chmod = subprocess.Popen(shlex.split("sudo chmod 777 /srv"), stdout=devnull, stderr=devnull)
+        chmod = subprocess.Popen(shlex.split("sudo chmod 777 /srv"), stdout=devnull)
         chmod.wait()
-        os.makedirs("/srv/src")
-        clone = subprocess.Popen(shlex.split("git clone http://github.com/saltpy/dotfiles " + SRC), stdout=devnull, stderr=devnull)
+        assert chmod.returncode == 0
+        mkdirsp = subprocess.Popen(shlex.split("mkdir -p /srv/src"), stdout=devnull)
+        mkdirsp.wait()
+        assert chmod.returncode == 0
+        clone = subprocess.Popen(shlex.split("git clone http://github.com/saltpy/dotfiles " + SRC), stdout=devnull)
         clone.wait()
+        assert clone.returncode == 0
+        plug = subprocess.Popen(shlex.split("curl -fLo " + HOME + "/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"), stdout=devnull)
+        plug.wait()
+        assert plug.returncode == 0
 
     for fp in glob.glob(os.path.join(SRC, "*")):
         print(fp)
